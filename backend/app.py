670ea841a707bd4a0be48b3d721e27576fdb03ea
andrea1932734 <andreacarovelli@gmail.com>
2025-05-13 11:58:07 +0000

    frontend-testato

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = flask(__name__)
+CORS(app)
+
+@app.route("/api/data")
+def data():
+    return jsonify({"messaggio":"funziona dio perdona regess"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000)
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  messaggio = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:5000/api/data').subscribe(data => {
+      this.messaggio = data.messaggio;
+    });
+  }
 }
